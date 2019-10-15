8a2fe474cf19450db92987d8c5bce7535acb8bb2
Ben Vaughn <bgvaughn@iu.edu>
2019-10-15 13:58:28 -0400

    trying to fix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, logging, open_color, arcade
+#!/usr/local/env python3
+import sys, logging, os, open_color, arcade
 
 #check to make sure we are running the right version of Python
 version = (3,7)
@@ -8,10 +9,49 @@ assert sys.version_info >= version, "This script requires at least Python {0}.{1
 logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = ""
+# Constants
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_TITLE = "The Space Between Us!"
+MARGIN = 50
 
+SHIP_HP = 100
+SHIP_SCALE = 0.5
+
+NUM_ENEMIES = 5
+ENEMY_SCALE = 0.5
+ENEMY_MIN_Y = 400
+ENEMY_MIN_HP = 10
+ENEMY_MAX_HP = 50
+ENEMY_MIN_MASS = 10
+ENEMY_MAX_MASS = 100
+ENEMY_ACCELERATION = 10
+
+
+class Player(arcade.Sprite):
+    def __init__(self, image, scale, x, y):
+        super().__init__(image, scale)
+        self.center_x = x
+        self.center_y = y
+        self.dx = 0
+        self.dy = 0
+        self.target_x = x
+        self.target_y = y
+
+class Enemy(arcade.Sprite):
+    def __init__(self, x, y, mass, hp):
+        sprites = ['Sprites X2.spaceShips_001', 'Sprites X2.spaceShips_002', 'Sprites X2.spaceShips_003', 'Sprites X2.spaceShips_004', 'Sprites X2.spaceShips_005', 'Sprites X2.spaceShips_006', 'Sprites X2.spaceShips_007', 'Sprites X2.spaceShips_008', 'Sprites X2.spaceShips_009']
+        sprites = random.choice(sprites)
+        super().__init__("assets/{}.png".format(sprite), ENEMY_SCALE)
+        self.center_x = x
+        self.center_y = y
+        self.hp = hp
+        self.mass = mass
+        self.dx = 0
+        self.dy = 0
+        self.target_x = x
+        self.target_y = y
+        self.acceleration =ENEMY_ACCELERATION / self.mass
 
 class Window(arcade.Window):
 
@@ -19,17 +59,34 @@ class Window(arcade.Window):
 
         # Call the parent class's init function
         super().__init__(width, height, title)
-        file_path 
-
-        
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
         self.set_mouse_visible(True)
 
-        arcade.set_background_color(open_color.black)
+        arcade.set_background_color(open_color.blue_4)
+
+        self.player_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+        self.enemy_bullet_list = arcade.SpriteList()
 
 
 
     def setup(self):
-        pass 
+        self.player = True
+        self.score = 0.0
+        self.hp = SHIP_HP
+        
+
+        self.player = Player("assets/player.png", SHIP_SCALE,400,50)
+        self.player_list.append(self.player)
+        for e in range(NUM_ENEMIES):
+            x = random.randint(MARGIN,SCREEN_WIDTH-MARGIN)
+            y = random.randint(SCREEN_HEIGHT-ENEMY_MIN_Y,SCREEN_HEIGHT-MARGIN)
+            hp = random.randint(ENEMY_MIN_HP,ENEMY_MAX_HP)
+            mass = random.randint(ENEMY_MIN_MASS,ENEMY_MAX_MASS)
+            enemy = Enemy(x, y, mass, hp)
+            self.enemy_List.append(enemy)
 
     def update(self, delta_time):
         pass
@@ -37,6 +94,10 @@ class Window(arcade.Window):
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
+        self.player_list.draw()
+        self.enemy_list.draw()
+        self.bullet_list.draw()
+        self.enemy_bullet_list.draw()
 
 
 
